efa921247d529aee73138780e5745160e883cb58
Alexander Wamboldt <alexander.wamboldt@gmail.com>
2025-04-02 15:34:09 +0200

    moving betting out of song mart and into its own

--- models/mart/mart_song.sql
+++ models/mart/mart_song.sql
@@ -3,15 +3,8 @@ WITH relationship_ratio AS
     AVG(point_ratio) AS point_ratio
     FROM {{ref('mart_relationship')}}
     GROUP BY to_country)
-, betting_ratio AS
-	(SELECT YEAR
-	, country_name
-	, AVG(betting_score) AS betting_odds
-	FROM {{ref('prep_betting')}}
-	GROUP BY YEAR, country_name)
 SELECT ps.*
 	, rr.point_ratio
 	, br.betting_odds
 FROM {{ref('prep_songs')}} AS ps
 JOIN relationship_ratio AS rr ON ps.country = rr.to_country
-JOIN betting_ratio AS br ON ((ps.YEAR = br.YEAR) AND (ps.country = br.country_name))
