c98c656acf20c5941a889a10d036d5b95bca9efa
Alexander Wamboldt <alexander.wamboldt@gmail.com>
2025-04-04 11:40:59 +0200

    point ration in song

--- models/mart/mart_relationship.sql
+++ models/mart/mart_relationship.sql
@@ -5,6 +5,7 @@ WITH points AS (SELECT prep_votes.relationship
 		, prep_votes.total_points
 		, (CASE WHEN prep_votes.YEAR >=2016 THEN prep_votes.total_points/24.0
 		ELSE prep_votes.total_points/12.0 END) AS point_ratio
+		, (CASE WHEN point_ratio = 1 THEN 1 ELSE 0 END) AS first_place
 		FROM {{ref('prep_votes')}}
 		WHERE round='final'),
 	average AS (SELECT relationship
@@ -13,5 +14,6 @@ WITH points AS (SELECT prep_votes.relationship
 		GROUP BY relationship)
 SELECT points.*
 , average.avg_point_ratio
+, average.total_firsts
 FROM points
 JOIN average ON points.relationship = average.relationship
