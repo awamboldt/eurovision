5db00a32b143565f460d687a9edd5958572af736
Alexander Wamboldt <alexander.wamboldt@gmail.com>
2025-04-09 16:13:13 +0200

    added placement to prep_betting

--- models/prep/prep_betting.sql
+++ models/prep/prep_betting.sql
@@ -1,2 +1,4 @@
-select *
-from {{source('extracted_data', 'betting')}}
+SELECT B.*
+, m.final_place
+FROM {{source('extracted_data', 'betting')}} AS b
+JOIN {{ref('mart_song')}} AS m ON m.YEAR = b.YEAR AND m.artist_name = b.performer
