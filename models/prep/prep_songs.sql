4820a55e8e560924bc44be529229b43230e72e95
Alexander Wamboldt <alexander.wamboldt@gmail.com>
2025-04-09 16:02:03 +0200

    clusters?!?!?!

--- models/prep/prep_songs.sql
+++ models/prep/prep_songs.sql
@@ -1,4 +1,4 @@
 select s.*
 , c.normal_cluster
 from {{source('extracted_data', 'songs_info_with_2013')}} AS s
-JOIN {{source('extracted_data', 'clusters')}} AS c ON (c.year = s.year AND c.artist_name = c.artist_name)
+JOIN {{source('extracted_data', 'clusters')}} AS c ON (c.year = s.year AND s.artist_name = c.artist_name)
