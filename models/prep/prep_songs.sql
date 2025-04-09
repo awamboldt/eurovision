fb4a367df2a859740ca52325a59c34a2e05fbdc3
Alexander Wamboldt <alexander.wamboldt@gmail.com>
2025-04-09 15:52:02 +0200

    added clusters to mart_song

--- models/prep/prep_songs.sql
+++ models/prep/prep_songs.sql
@@ -1,2 +1,4 @@
-select *
-from {{source('extracted_data', 'songs_info_with_2013')}}
+select s.*
+, c.normal_cluster
+from {{source('extracted_data', 'songs_info_with_2013')}} AS s
+JOIN {{source('extracted_data', 'clusters')}} AS c ON c.year = s.year AND c.artist_name = c.artist_name
