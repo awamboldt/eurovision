b06d7c98d2becc49ce15b77cc51f3c869b9f1dff
Alexander Wamboldt <alexander.wamboldt@gmail.com>
2025-04-11 14:51:22 +0200

    changed prep_betting join

--- models/prep/prep_betting.sql
+++ models/prep/prep_betting.sql
@@ -1,4 +1,4 @@
 SELECT B.*
 , m.final_place
 FROM {{source('extracted_data', 'betting')}} AS b
-JOIN {{ref('mart_song')}} AS m ON m.YEAR = b.YEAR AND m.artist_name = b.performer
+JOIN {{ref('mart_song')}} AS m ON m.YEAR = b.YEAR AND m.country = b.country_name
